a5d921b6fc45a7e2165885bfd1097b0a9c37a1c0
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-09-22 05:06:53 +0100

    Simple calculator program

--- simple_calculator.py
+++ simple_calculator.py
@@ -7,16 +7,16 @@ num1, operator, num2 = input("Enter Calculation: ").split()
 num1 = int(num1)
 num2 = int(num2)
 if operator == "-":
-    print("Equal: {} + {} = {}".format(num1, num2, num1-num2))
+    print("{} - {} = {}".format(num1, num2, num1-num2))
 elif operator == "+":
-    print("Equal: {} + {} = {}".format(num1, num2, num1+num2))
+    print("{} + {} = {}".format(num1, num2, num1+num2))
 elif operator == "*":
-    print("Equal: {} + {} = {}".format(num1, num2, num1*num2))
+    print("{} * {} = {}".format(num1, num2, num1*num2))
 elif operator == "/":
-    print("Equal: {} + {} = {}".format(num1, num2, num1/num2))
+    print("{} / {} = {}".format(num1, num2, num1/num2))
 elif operator == "%":
-    print("Equal: {} + {} = {}".format(num1, num2, num1%num2))
+    print("{} % {} = {}".format(num1, num2, num1%num2))
 elif operator == "**":
-    print("Equal: {} + {} = {}".format(num1, num2, num1**num2))
+    print("{} ** {} = {}".format(num1, num2, num1**num2))
 else:
     print("Use either - + * / % or ** next time")
